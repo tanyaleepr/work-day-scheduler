0ebb4d94d07a9576ec6722f2bcabdacc7583201b
tanyaleepr <tanyaleepr@gmail.com>
2022-01-21 00:17:28 -0600

    variable for moment.js in hours, input, and span

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ var currentHour = moment().format('h:mm:ss a');
 
 //This section is for the hour variables in the schedule
 
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
