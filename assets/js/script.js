0d125ae199cd859cbda8afd4c2d6074fd03502f3
tanyaleepr <tanyaleepr@gmail.com>
2022-01-21 00:10:47 -0600

    variables from Moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-
+//This section is for the Momentum, I mean Moment.js 
 
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
+
+//This section is for the hour variables in the schedule
+
